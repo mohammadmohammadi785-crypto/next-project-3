f58e5449a4b7c7278ce500106f956ff23bf75855
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2025-12-03 19:58:40 +0430

    Changes added

--- src/app/blogs/route.ts
+++ src/app/blogs/route.ts
@@ -1,4 +1,15 @@
 import { blog } from "./posts";
 export function GET() {
-  return new Response("Hello From Blogs Api");
+  return new Response(JSON.stringify(blog));
+}
+export async function POST(request: Request) {
+  const data = await request.json();
+  const newBlog = {
+    id: data.id,
+    text: data.text,
+  };
+  blog.push();
+  return new Response(JSON.stringify(newBlog), {
+    headers: { "content-type": "application/json" },
+  });
 }
