93b1d692121b59c612a3e1983b760c2f6ed00a4d
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2025-12-03 12:13:24 +0430

    Changes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-import React from "react";
-
 async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
@@ -8,15 +5,7 @@ async function Home() {
       <h1 className="font-bold text-4xl text-center">
         Welcome to the Home Page
       </h1>
-      <Link className="px-6 py-4 font-bold text-2xl" href="/articles/1?lang=en">
-        read article in english
-      </Link>
-      <Link className="px-6 py-4 font-bold text-2xl" href="/articles/1?lang=fa">
-        read article in persion
-      </Link>
-      <Link className="px-6 py-4 font-bold text-2xl" href="/articles/1?lang=pa">
-        read article in pashto
-      </Link>
+      
     </div>
   );
 }
