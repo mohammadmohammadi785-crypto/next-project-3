756928213d8d547ff60a9d5bc1049791f276a7d8
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2025-12-04 20:23:20 +0430

    Changes added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,7 @@
 export default function loading() {
   return (
-    <div className="flex absolute z-20 top-0 right-0 justify-center w-full h-screen items-center">
+    <div className="flex absolute top-0 flex-col right-0 justify-center w-full h-screen items-center">
+      <h1 className="text-3xl m-2">loading</h1>
       <div className="animate-spin w-20 h-20 border-dotted border-white border-8 rounded-full"></div>
     </div>
   );
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 function NotFound() {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-4xl text-gray-500">This Page is Not Found</h1>
+    <div className="flex absolute w-full h-screen top-0 right-0 justify-center items-center">
+      <h1 className="text-4xl text-white">This Page is Not Found</h1>
     </div>
   );
 }
