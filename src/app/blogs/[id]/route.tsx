6c3c71decf7bbaae5b8e31258de1b13859ff2a35
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2025-12-20 10:52:33 +0430

    Changes added

--- src/app/blogs/[id]/route.tsx
+++
@@ -1,9 +0,0 @@
-import { blog } from "../posts";
-export async function GET(
-  _request: Request,
-  { params }: { params: Promise<{ id: string }> }
-) {
-  const { id } = await params;
-  const blogs = blog.find((blog) => blog.id === parseInt(id));
-  return Response.json(blogs);
-}
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function loading() {
   return (
     <div className="flex flex-col absolute top-0 right-0 justify-center w-full h-screen items-center">
-      <h1 className="text-5xl my-2">loading</h1>
+      {/* <h1 className="text-5xl my-2">loading</h1> */}
       <div className="animate-spin w-40 h-40 border-dotted border-20 border-white rounded-full"></div>
     </div>
   );
